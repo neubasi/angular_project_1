890fc9b9d1a97edc404bb5a733126be406de48ea
neubasi <neubauersim@gmail.com>
2018-05-21 12:37:17 +0200

    asd

--- angular1/src/app/app.module.ts
+++ angular1/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MyComp1Component } from './my-comp1/my-comp1.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComp1Component
   ],
   imports: [
     BrowserModule,FormsModule
